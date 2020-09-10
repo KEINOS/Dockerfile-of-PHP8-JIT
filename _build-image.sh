35feeb059105986b6b0ab712d5540eba3268d970
KEINOS <github@keinos.com>
2020-09-11 01:58:56 +0900

    feat: Alpine v3.12.0 Build:

--- _build-image.sh
+++ _build-image.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -eu
 
 cat <<'HEREDOC'
 ===============================================================================
@@ -6,59 +6,69 @@ cat <<'HEREDOC'
   Supporting architectures:
     ARM v6,v7 (RaspberryPi) and x86_64 (macOS, Windows Intel/AMD machines)
 ===============================================================================
-This script:
-1. Builds Docker image for ARM v6, ARM v7 and AMD64 (compatible to Intel/x86_64)
-   architecture.
+This script will:
+
+1. Builds Docker images for ARM v6, ARM v7 and x86_64 (Intel/AMD64 compatible)
+   architectures.
 2. Pushes to Docker Hub the images made above.
 3. Creates a "latest" manifest file which includes the above images and pushes
    to Docker Hub as well.
 
-- Requirements:
-    1. Experimental option must be enabled. (buildx command must be available to use as well)
-    2. When running "docker buildx ls", the below platforms must be listed:
-        - linux/arm/v6
-        - linux/arm/v7
-        - linux/arm64
-        - linux/amd64
+Requirements:
+
+1. Experimental option of Docker must be enabled.("buildx" command must be
+   available to use as well)
+2. When running "docker buildx ls", the below platforms must be listed:
+    - linux/arm/v6
+    - linux/arm/v7
+    - linux/arm64
+    - linux/amd64
 
 ===============================================================================
 
 HEREDOC
 
 [ 'true' = $(docker version --format {{.Client.Experimental}}) ] || {
-   echo 'Docker daemon not in experimental mode.'
-   exit 1
+    echo 'Docker daemon not in experimental mode.'
+    exit 1
 }
 
 # -----------------------------------------------------------------------------
 #  Common Variables
 # -----------------------------------------------------------------------------
-NAME_IMAGE='keinos/php8-jit'
-PATH_FILE_VER_INFO='VERSION_IMAGE_BASE.txt'
-NAME_BUILDER=mybuilder
+PHP_VERSION='8.0.0-dev'
 BUILD_ID_CURRENT=$(date '+%Y%m%d')
+NAME_IMAGE='keinos/php8-jit'
+NAME_BUILDER=mybuilder
+NUM_CORE=$(get_core_number)
+PATH_FILE_VER_INFO='VERSION_IMAGE_BASE.txt'
+PATH_DIR_SELF=$(cd "$(dirname "${BASH_SOURCE:-$0}")" && pwd)
+RELESE_TAG="${PHP_VERSION}-build-${BUILD_ID_CURRENT}"
 
 # -----------------------------------------------------------------------------
 #  Functions
 # -----------------------------------------------------------------------------
 
-function build_push_pull_image () {
+function build_push_pull_image() {
     echo "- Remove image"
     docker image rm -f "${NAME_BASE}:${NAME_TAG}"
-    echo "- BUILDING ${NAME_PLATFORM}"
+    echo "- Building image: ${NAME_PLATFORM}"
     docker buildx build \
         --build-arg NAME_BASE=$NAME_BASE \
         --build-arg NAME_TAG=$NAME_TAG \
+        --build-arg BUILD_ID=$BUILD_ID_CURRENT \
+        --build-arg PHP_VERSION=$PHP_VERSION \
+        --build-arg RELESE_TAG=$RELESE_TAG \
         --platform $NAME_PLATFORM \
         -t "${NAME_IMAGE}:${NAME_TAG}" \
-        --push . && \
-    echo "  PULLING BACK: ${NAME_IMAGE}:${NAME_TAG}" && \
-    docker pull "${NAME_IMAGE}:${NAME_TAG}"
+        --push . &&
+        echo "  Pulling back image: ${NAME_IMAGE}:${NAME_TAG}" &&
+        docker pull "${NAME_IMAGE}:${NAME_TAG}"
 
     return $?
 }
 
-function create_builder () {
+function create_builder() {
     echo '- Create builder: ' $1
     docker buildx ls | grep $1 1>/dev/null
     [ $? -ne 0 ] && {
@@ -68,7 +78,7 @@ function create_builder () {
     return $?
 }
 
-function create_manifest () {
+function create_manifest() {
     echo '- Removing image from local:'
     docker image rm --force $1 2>/dev/null 1>/dev/null
     echo "- Creating manifest for: $1"
@@ -78,7 +88,23 @@ function create_manifest () {
     return $?
 }
 
-function login_docker () {
+function get_core_number() {
+    which nproc 2>&1 && {
+        echo $(nproc)
+    } || {
+        echo $(sysctl -n hw.logicalcpu_max)
+    }
+}
+
+function indent_stdin() {
+    indent='    '
+    while read line; do
+        echo "${indent}${line}"
+    done
+    echo
+}
+
+function login_docker() {
     echo -n '- Login to Docker: '
     docker login 2>/dev/null 1>/dev/null || {
         echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin || {
@@ -89,7 +115,7 @@ function login_docker () {
     echo 'OK'
 }
 
-function rewrite_variant_manifest () {
+function rewrite_variant_manifest() {
     echo "- Re-writing variant to: $3"
     docker manifest annotate $1 $2 --variant $3
 
@@ -100,12 +126,6 @@ function rewrite_variant_manifest () {
 #  Main
 # -----------------------------------------------------------------------------
 
-# Load current Alpine version info and build ID
-source ./$PATH_FILE_VER_INFO
-VERSION_OS=$VERSION_ID
-echo '- Current Alpine version:' $VERSION_OS
-echo '- Current Build ID:' $BUILD_ID
-
 # Setup docker for multi-arc
 login_docker
 
@@ -115,6 +135,10 @@ echo '- Start build:'
 docker buildx use $NAME_BUILDER
 docker buildx inspect --bootstrap
 
+# =============
+#  BUILD IMAGE
+# =============
+# For debug reasons do not loop the below build steps
 # Build ARMv6
 NAME_BASE='keinos/alpine'
 NAME_TAG='armv6'
@@ -139,7 +163,9 @@ NAME_TAG='amd64'
 NAME_PLATFORM='linux/amd64'
 build_push_pull_image
 
-# Inspect built
+# ======================
+#  INSPECT BUILT IMAGES
+# ======================
 echo "- Inspect built image of: ${NAME_IMAGE}"
 docker buildx imagetools inspect $NAME_IMAGE
 
@@ -147,27 +173,34 @@ echo '- Switch back builder to default:'
 docker buildx stop $NAME_BUILDER
 docker buildx use default
 
+# =================
+#  CREATE MANIFEST
+# =================
 # Create manifest for "latest" tag
 LIST_IMAGE_INCLUDE="$NAME_IMAGE:armv6 $NAME_IMAGE:armv7 $NAME_IMAGE:arm64 $NAME_IMAGE:amd64"
 
 echo "- Creating manifest for image: ${NAME_IMAGE} with: latest tag"
 NAME_IMAGE_AND_TAG="${NAME_IMAGE}:latest"
-create_manifest  $NAME_IMAGE_AND_TAG "$LIST_IMAGE_INCLUDE"
+create_manifest $NAME_IMAGE_AND_TAG "$LIST_IMAGE_INCLUDE"
 
 rewrite_variant_manifest $NAME_IMAGE_AND_TAG $NAME_IMAGE:armv6 v6l
 rewrite_variant_manifest $NAME_IMAGE_AND_TAG $NAME_IMAGE:armv7 v7l
 
-docker manifest inspect $NAME_IMAGE_AND_TAG && \
-docker manifest push $NAME_IMAGE_AND_TAG --purge
+docker manifest inspect $NAME_IMAGE_AND_TAG &&
+    docker manifest push $NAME_IMAGE_AND_TAG --purge
 
 # Create manifest list with current version
 echo "- Creating manifest for image: ${NAME_IMAGE} with: v${VERSION_OS} tag"
 NAME_IMAGE_AND_TAG="${NAME_IMAGE}:build_${BUILD_ID_CURRENT}"
 
-create_manifest  $NAME_IMAGE_AND_TAG "$LIST_IMAGE_INCLUDE"
+create_manifest $NAME_IMAGE_AND_TAG "$LIST_IMAGE_INCLUDE"
 
+# Re-write the variant for ARM6 and ARM7 architecture for RPIs
 rewrite_variant_manifest $NAME_IMAGE_AND_TAG $NAME_IMAGE:armv6 v6l
 rewrite_variant_manifest $NAME_IMAGE_AND_TAG $NAME_IMAGE:armv7 v7l
 
-docker manifest inspect $NAME_IMAGE_AND_TAG && \
-docker manifest push $NAME_IMAGE_AND_TAG --purge
+# ===============
+#  PUSH MANIFEST
+# ===============
+docker manifest inspect $NAME_IMAGE_AND_TAG &&
+    docker manifest push $NAME_IMAGE_AND_TAG --purge
---
+++ run-updater.sh
@@ -0,0 +1,188 @@
+#!/usr/bin/env bash
+# =============================================================================
+#  Image Updater of Docker for PHP 8.0 w/Alpine Base Image
+# =============================================================================
+# This script checks the latest Alpine docker image version and updates the
+# Docker images.
+#
+# NOTE:
+#   - This script must run on local and only was tested with macOS(Mojave).
+#   - Exits with a "0"(zero) staus if no update found.
+#
+# Update steps:
+#   1. Pull alpine:latest image.
+#   2. Gets the os-release version of the image above.
+#   3. Compares the version between alpine:latest and keinos/alpine:latest.
+#   4. Re-writes the version info of the Dockerfile.
+#   5. Git add, commit and push if updated.
+
+# -----------------------------------------------------------------------------
+#  Functions
+# -----------------------------------------------------------------------------
+
+function commit_push_git() {
+  git status | grep 'nothing to commit' && {
+    return 0
+  }
+  # Updating git
+  echo '- GIT: Committing and pushing to GitHub ...'
+  git add . &&
+    git commit -m "feat: Alpine v${VERSION_OS_NEW} Build: ${BUILD_ID}" &&
+    git tag "${TAG_RELEASED_NEW}" &&
+    git push --tags &&
+    git push origin
+  [ $? -ne 0 ] && {
+    echo >&2 '* Failed commit and push'
+    exit 1
+  }
+}
+
+function get_version_alpine_latest() {
+  docker pull keinos/alpine >/dev/null
+  docker run --rm -i keinos/alpine cat /etc/os-release | grep VERSION_ID | sed -e 's/[^0-9\.]//g'
+}
+
+function get_version_php() {
+  docker run --rm "${1:-$NAME_IMAGE_DOCKER_LATEST}" php -r 'echo phpversion();'
+}
+
+function update_info_build() {
+  echo "Updating/over-writing ${NAME_FILE_BUILD_INFO}"
+  cat <<EOL >$PATH_FILE_BUILD_INFO
+VERSION_OS=${VERSION_OS_NEW}
+VERSION_PHP=${VERSION_PHP_NEW}
+ID_BUILD=${ID_BUILD_NEW}
+ID_SRC_ARCHIVED=${ID_SRC_ARCHIVED_NEW}
+TAG_RELEASED=${TAG_RELEASED_NEW}
+EOL
+}
+
+function update_php_info() {
+  docker run --rm "${1:-$NAME_IMAGE_DOCKER_LATEST}" php -i >$PATH_FILE_PHP_INFO
+}
+
+function update_php_modules() {
+  docker run --rm "${1:-$NAME_IMAGE_DOCKER_LATEST}" php -m >$PATH_FILE_EXT_INFO
+}
+
+function update_src_archive() {
+  name_tag_test='test:local'
+  ./_download-source.sh
+}
+
+function build_docker_for_smoke_test() {
+  echo '- Building test image'
+  docker system prune -a -f &&
+    docker build -t $name_tag_test . &&
+    update_php_info "$name_tag_test" &&
+    update_php_modules "$name_tag_test" &&
+    VERSION_PHP_NEW=$(get_version_php "$name_tag_test")
+}
+
+# -----------------------------------------------------------------------------
+#  Constants
+# -----------------------------------------------------------------------------
+NAME_FILE_BUILD_INFO='info-build.txt'
+NAME_FILE_PHP_INFO='info-phpinfo.txt'
+NAME_FILE_EXT_INFO='info-get_loaded_extensions.txt'
+NAME_IMAGE_DOCKER_LATEST='keinos/php8-jit:latest'
+
+VERSION_OS_NEW=$(get_version_alpine_latest)
+VERSION_PHP_NEW=$(get_version_php)
+ID_BUILD_NEW=$(date '+%Y%m%d')
+ID_SRC_ARCHIVED_NEW=$(date '+%Y%m%d')
+TAG_RELEASED_NEW="8.0.0-dev-build-${ID_BUILD_NEW}"
+
+PATH_DIR_SELF=$(cd "$(dirname "${BASH_SOURCE:-$0}")" && pwd)
+PATH_FILE_BUILD_INFO="${PATH_DIR_SELF:-.}/${NAME_FILE_BUILD_INFO}"
+PATH_FILE_PHP_INFO="${PATH_DIR_SELF:-.}/${NAME_FILE_PHP_INFO}"
+PATH_FILE_EXT_INFO="${PATH_DIR_SELF:-.}/${NAME_FILE_EXT_INFO}"
+
+# -----------------------------------------------------------------------------
+#  Setup
+# -----------------------------------------------------------------------------
+usage() {
+  echo "usage: ${0} [OPTIONS]"
+  echo
+  echo "OPTIONS:"
+  echo
+  echo "  help   This help."
+  echo "  force  Update even Alpine's version are the same."
+  echo
+}
+
+update_force=0 # Force update by default: 0=no 1=yes
+case "$1" in
+force)
+  update_force=1
+  ;;
+*)
+  update_force=0
+  ;;
+esac
+
+# Load current version info
+source ./$NAME_FILE_BUILD_INFO
+
+# Show current info
+[ "$update_force" -ne 0 ] && { echo '- MODE: Force update'; }
+echo '- Current Alpine version     :' ${VERSION_OS:-unknown} '-> Latest version:' ${VERSION_OS_NEW:-unknown}
+echo '- Current PHP version        :' ${VERSION_PHP:-unknown}
+echo '- Current build ID           :' ${ID_BUILD:-unknown}
+echo '- Current GitHub release tag :' ${TAG_RELEASED:-unknown}
+echo '- Current PHP source ID      :' ${ID_SRC_ARCHIVED:-unknown}
+
+# Compare OS version
+msg_update='Updataing ...'
+[ "$update_force" -eq 0 ] && {
+  [ "$VERSION_OS" = "$VERSION_OS_NEW" ] && {
+    echo 'No update found. Do nothing.'
+    usage
+    exit 0
+  }
+  msg_update='Newer OS version of the base image found. Updating ...'
+}
+
+[ "$ID_SRC_ARCHIVED" = "${ID_SRC_ARCHIVED_NEW}" ] || {
+  echo '---------------------------------------------------'
+  echo ' Updating Archive of PHP Source'
+  echo '---------------------------------------------------'
+  echo ' Archived date did not match. Updating ...'
+
+  update_src_archive || {
+    exit 1
+  }
+
+  build_docker_for_smoke_test || {
+    exit 1
+  }
+
+  update_info_build || {
+    echo 'Failed to update'
+    exit 1
+  }
+
+  commit_push_git
+
+  echo '* Now release the commit and upload the archive, signature and the key to the assets.'
+  echo '  Then re-run this script to build the real images.'
+  exit 0
+}
+
+# -----------------------------------------------------------------------------
+#  Main (Update)
+# -----------------------------------------------------------------------------
+echo $msg_update
+
+# Clear all the docker images and containers for stable build
+docker system prune -f -a
+
+# Build the images
+./_build-image.sh
+[ $? -ne 0 ] && {
+  echo >&2 "* Failed update: ${PATH_FILE_BUILD_INFO}"
+  exit 1
+}
+echo "- Updated"
+
+varsion_php=$(docker run --rm keinos/php8-jit:latest php -r 'echo phpversion();')
